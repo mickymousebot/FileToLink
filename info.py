c028d70907101bc18810900a81f9f85181b5eb52
Micky Mouse <techsashif+eufya@gmail.com>
2025-03-23 09:56:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,5 +32,5 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "techvjautobot")
 
 # Shortlink Info
 SHORTLINK = bool(environ.get('SHORTLINK', 'False')) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
+SHORTLINK_API = environ.get('SHORTLINK_API', '')
